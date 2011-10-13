ca0da9b6567b2241cc515e50cb9af1edbe0a721f
Zach Bora <zachbora@gmail.com>
2011-10-12 21:37:21 -0400

    Added configuration file and display who is OP or in creative mode

--- src/com/worldcretornica/ichatplayerlist/PlayerLoginListener.java
+++ src/com/worldcretornica/ichatplayerlist/PlayerLoginListener.java
@@ -2,7 +2,9 @@ package com.worldcretornica.ichatplayerlist;
 
 import net.TheDgtl.iChat.iChat;
 
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerListener;
 
@@ -16,13 +18,28 @@ public class PlayerLoginListener extends PlayerListener {
 	}
 	
 	@Override
+	public void onPlayerChat(PlayerChatEvent event) {
+		
+		Player player = event.getPlayer();
+		addPlayerToList(player);				
+		
+		super.onPlayerChat(event);
+	}
+	
+	@Override
 	public void onPlayerJoin(PlayerJoinEvent event) {
 		
 		Player player = event.getPlayer();
+		addPlayerToList(player);
 		
+		super.onPlayerJoin(event);
+	}
+	
+	
+	public void addPlayerToList(Player player)
+	{
 		((iChat) plugin.ichatplugin).info.addPlayer(player);
 		
-		//if(plugin.ichatapi.getPrefix(player) == null)
 		if(((iChat) plugin.ichatplugin).info.getKey(player, "prefix") == null)
 		{
 			plugin.logger.severe("Unable to get prefix");
@@ -34,16 +51,62 @@ public class PlayerLoginListener extends PlayerListener {
 				int lastcolor = prefix.lastIndexOf("&");
 				
 				String coloredname = "&" + prefix.charAt(lastcolor + 1) + player.getName();
+				String tabname = coloredname;
 				
-				String fixedname = ((iChat) plugin.ichatplugin).API.addColor(coloredname);
-				
-				player.setDisplayName(fixedname);
-				if (fixedname.length()>16)
-					player.setPlayerListName(fixedname.substring(0, 14) + "..");
-				else
-					player.setPlayerListName(fixedname);
+				if(plugin.mainconfig.getBoolean("ShowInTAB"))
+				{
+					tabname = checkOP(tabname, player);
+					tabname = ((iChat) plugin.ichatplugin).API.addColor(tabname);
+					if (tabname.length()>16)
+						player.setPlayerListName(tabname.substring(0, 14) + "..");
+					else
+						player.setPlayerListName(tabname);
+				}
+				if(plugin.mainconfig.getBoolean("ShowInDisplayName"))
+				{
+					coloredname = checkOP(coloredname, player);
+					coloredname = ((iChat) plugin.ichatplugin).API.addColor(coloredname);
+					player.setDisplayName(coloredname);
+				}				
 			}
 		}
-		super.onPlayerJoin(event);
 	}
+	
+	public String checkOP(String name, Player player)
+	{
+		if(plugin.mainconfig.getBoolean("ShowStatusOP"))
+		{
+			if(player.isOp())
+			{
+				name = plugin.mainconfig.getString("OPSymbol") + name;
+				
+				if(plugin.mainconfig.getBoolean("ShowBothStatus"))
+				{
+					name = checkCreative(name, player);
+				}
+			}else{
+				name = checkCreative(name, player);
+			}
+			
+			return name;
+		}else{
+			return checkCreative(name, player);
+		}
+	}
+	
+	public String checkCreative(String name, Player player)
+	{
+		if(plugin.mainconfig.getBoolean("ShowStatusCreative"))
+		{
+			if(player.getGameMode() == GameMode.CREATIVE)
+			{
+				name = plugin.mainconfig.getString("CreativeSymbol") + name;
+			}
+			
+			return name;
+		}else{
+			return name;
+		}
+	}
+	
 }
--- src/com/worldcretornica/ichatplayerlist/iChatPlayerList.java
+++ src/com/worldcretornica/ichatplayerlist/iChatPlayerList.java
@@ -1,10 +1,15 @@
 package com.worldcretornica.ichatplayerlist;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.logging.Logger;
 
 import net.TheDgtl.iChat.iChat;
 import net.TheDgtl.iChat.iChatAPI;
 
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.Event;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
@@ -21,6 +26,8 @@ public class iChatPlayerList extends JavaPlugin {
 	public final Logger logger = Logger.getLogger("Minecraft");
 	
 	public Plugin ichatplugin;
+	
+	public YamlConfiguration mainconfig;
 	
 	public iChatAPI ichatapi;
 	
@@ -41,14 +48,69 @@ public class iChatPlayerList extends JavaPlugin {
 		{
 			ichatapi = ((iChat) ichatplugin).API;
 			
+			loadConfig();
+			
 			PluginManager pm = getServer().getPluginManager();
-			pm.registerEvent(Event.Type.PLAYER_JOIN, this.playerloginlistener, Event.Priority.Normal, this);	
+			pm.registerEvent(Event.Type.PLAYER_JOIN, this.playerloginlistener, Event.Priority.Normal, this);
+			pm.registerEvent(Event.Type.PLAYER_CHAT, this.playerloginlistener, Event.Priority.High, this);
 			
 			this.logger.info(pdfdescription + " version " + pdfversion + " iChat " + ichatplugin.getDescription().getVersion() + " detected");
 			this.logger.info(pdfdescription + " version " + pdfversion + " is enabled");
 		}else{
 			this.logger.severe(pdfdescription + " version " + pdfversion + " iChat not found!");
 			this.setEnabled(false);
+		}
+	}
+	
+	
+	private void loadConfig()
+	{
+		mainconfig = new YamlConfiguration();
+		
+		File file = new File(this.getDataFolder(), "config.yml");
+		
+		if(file.exists())
+		{
+			try {
+				mainconfig.load(file);
+			} catch (FileNotFoundException e) {
+				logger.severe("[" + pdfdescription + "] File not found: " + e.getMessage());
+			} catch (IOException e) {
+				logger.severe("[" + pdfdescription + "] IO Error: " + e.getMessage());
+			} catch (InvalidConfigurationException e) {
+				logger.severe("[" + pdfdescription + "] Invalid configuration: " + e.getMessage());
+			} catch (NullPointerException e){
+				logger.severe("[" + pdfdescription + "] Null pointer: ");
+			}
+			
+			if (!mainconfig.isBoolean("ShowStatusOP"))
+				mainconfig.set("ShowStatusOP", true);
+			if (!mainconfig.isBoolean("ShowStatusCreative"))
+				mainconfig.set("ShowStatusCreative", true);
+			if (!mainconfig.isBoolean("ShowInTAB"))
+				mainconfig.set("ShowInTAB", true);
+			if (!mainconfig.isBoolean("ShowInDisplayName"))
+				mainconfig.set("ShowInDisplayName", true);
+			if (!mainconfig.isBoolean("ShowBothStatus"))
+				mainconfig.set("ShowBothStatus", false);
+			if (!mainconfig.isString("OPSymbol"))
+				mainconfig.set("OPSymbol", "*");
+			if (!mainconfig.isString("CreativeSymbol"))
+				mainconfig.set("CreativeSymbol", "~");
+		}else{
+			mainconfig.set("ShowStatusOP", true);
+			mainconfig.set("ShowStatusCreative", true);
+			mainconfig.set("ShowInTAB", true);
+			mainconfig.set("ShowInDisplayName", true);
+			mainconfig.set("ShowBothStatus", false);
+			mainconfig.set("OPSymbol", "*");
+			mainconfig.set("CreativeSymbol", "~");
+		}
+
+		try {
+			mainconfig.save(file);
+		} catch (IOException e) {
+			logger.severe("[" + pdfdescription + "] IO Error: " + e.getMessage());
 		}
 	}
 
